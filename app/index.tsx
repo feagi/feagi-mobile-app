b60529461e81bee50e2de363fa52ed0cb577cec8
jebediahtoblerone <xeh606@gmail.com>
2025-02-20 16:09:59 -0500

    made part of the unplugged screen
    title

--- app/index.tsx
+++ app/index.tsx
@@ -107,7 +107,7 @@ export default function Index() {
 
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>{number}</Text>
-      <TextInput
+      <TextInput> </TextInput>
       onChangeText={onChangeText}
       value={text}
 
